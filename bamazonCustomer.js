45a65e235ec738ac3623a97e813298ed592aacc6
Chynna Johnson <chynnajohnson@Chynnas-MBP.attlocal.net>
2019-05-30 13:14:49 -0500

    inserted after connection function and display product function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,3 +9,33 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log('connected as id' + connection.threadId);
+});
+
+function afterConnection() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+    });
+
+var displayProducts = function () {
+    var query = "Select * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        var displayTable = new Table({
+            head: ["Item ID", "Product Name", "Catergory", "Price", "Quantity"],
+            colWidths: [10, 25, 25, 10, 14]
+        });
+        for (var i = 0; i < res.length; i++) {
+            displayTable.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        }
+        console.log(displayTable.toString());
+        purchasePrompt();
+    });
+
+}
