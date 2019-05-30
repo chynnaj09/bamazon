933aa740fc08672555b75c4f562e1785800a8862
Chynna Johnson <chynnajohnson@Chynnas-MBP.attlocal.net>
2019-05-30 13:28:37 -0500

    finished javascript,starting node

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -39,3 +39,43 @@ var displayProducts = function () {
     });
 
 }
+
+    function purchasePrompt() {
+        inquirer.prompt([
+            {
+                name: "ID",
+                type: "input",
+                message: "Please enter Item ID you like to purhcase.",
+                filter: Number
+            },
+            {
+                name: "Quantity",
+                type: "input",
+                message: "How many items do you wish to purchase?",
+                filter: Number
+            },
+
+        ]).then(function (answers) {
+            var quantityNeeded = answers.Quantity;
+            var IDrequested = answers.ID;
+            purchaseOrder(IDrequested, quantityNeeded);
+        });
+    };
+
+    function purchaseOrder(ID, amtNeeded) {
+        connection.query('Select * FROM products WHERE item_id = ' + ID, function (err, res) {
+            if (err) { console.log(err) };
+            if (amtNeeded <= res[0].stock_quantity) {
+                var totalCost = res[0].price * amtNeeded;
+                console.log("Good news your order is in stock!");
+                console.log("Your total cost for " + amtNeeded + " " + res[0].product_name + " is " + totalCost + " Thank you!");
+
+                connection.query("UPDATE products SET stock_quantity = stock_quantity - " + amtNeeded + "WHERE item_id = " + ID);
+            } else {
+                console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "to complete your order.");
+            };
+            displayProducts();
+        });
+    };
+}
+    displayProducts(); 
